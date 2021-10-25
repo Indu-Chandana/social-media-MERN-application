b1133439e893c5e48d2d43f7a5130375057a1098
I.Chandana <72302495+Indu-Chandana@users.noreply.github.com>
2021-10-25 18:49:46 +0530

    oct25-001

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Grow, Container, AppBar, Typography, Grid } from '@mui/material';
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from './styles'
-import { useDispatch } from "react-redux";
-import { getPosts } from './actions/posts'
-const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const[currentId, setCurrentId] = useState(null);
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [dispatch]);
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-    return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                {/* <img className={classes.image} src="https://purepng.com/public/uploads/thumbnail//google-stadia-logo-hd4.png" alt="memories" height="60" width="60" /> */}
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-    );
-}
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+    </Container>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ client/src/actions/auth.js
@@ -0,0 +1,10 @@
+import * as api from '../api';
+import { AUTH } from '../constans/actionTypes';
+
+export const signin = (formData, history) => async (dispatch) => {  //this async use fromredux-thunk
+    try {
+        
+    } catch (error) {
+        
+    }
+}
